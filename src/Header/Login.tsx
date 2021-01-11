2917c229d686145f6caf1dc6d1ddfad5abb43d77
Slava <slava91petrushin@yandex.ru>
2021-01-11 20:53:19 +0300

    update Login

--- src/Header/Login.tsx
+++ src/Header/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { API_KEY_3, API_URL } from '../api/api';
 
-const apiAuthentication = (url: string, option = {}) => {
+const apiAuthentication = (url: string = "", option = {}) => {
 	return new Promise((resolve, reject) => {
 			fetch(url, option)
 				.then(response => {
@@ -13,8 +13,7 @@ const apiAuthentication = (url: string, option = {}) => {
 				})
 				.then(data => resolve(data))
 				.catch(response  => {
-					response.json().then((error: any) => {
-
+					response.json().then((error: string) => {
 						reject(error);
 					});
 				})
@@ -22,49 +21,41 @@ const apiAuthentication = (url: string, option = {}) => {
 }
 
 class Login extends React.Component {
-	sendPromises = async () => {
-		try{
-			let requestToken: string;
-
-			const authentication = await  fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`);
-			if(authentication.status > 400) throw new Error ("Error authentication");
-			const authenticationResult = await  authentication.json();
-			requestToken = authenticationResult.request_token;
-
-			const validateWithLogin = await fetch(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`, {
-				method: "POST",
-				mode: "cors",
-				headers: {
-					'Content-Type': 'application/json;charset=utf-8'
-				},
-				body: JSON.stringify({
-					"username": "slavaPetrushin",
-					"password": "",
-					"request_token": requestToken
+	sendPromises = () => {
+		apiAuthentication(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
+			.then((data: any) => {
+				return apiAuthentication(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`, {
+					method: "POST",
+					mode: "cors",
+					headers: {
+						'Content-Type': 'application/json;charset=utf-8'
+					},
+					body: JSON.stringify({
+						"username": "slavaPetrushin",
+						"password": "488661632sla",
+						"request_token": data.request_token
+					})
 				})
 			})
-			if(validateWithLogin.status > 400) throw new Error ("Error validateWithLogin");
-
-			const validateWithLoginResult = await  validateWithLogin.json();
-			const {success, request_token} = validateWithLoginResult;
-
-			const session = await fetch(`${API_URL}/authentication/session/new?api_key=${API_KEY_3}`, {
-				method: "POST",
-				mode: "cors",
-				headers: {
-					'Content-Type': 'application/json;charset=utf-8'
-				},
-				body: JSON.stringify({
-					"success": success,
-					"request_token": request_token
+			.then((data: any) => {
+				return apiAuthentication(`${API_URL}/authentication/session/new?api_key=${API_KEY_3}`, {
+					method: "POST",
+					mode: "cors",
+					headers: {
+						'Content-Type': 'application/json;charset=utf-8'
+					},
+					body: JSON.stringify({
+						"success": data.success,
+						"request_token": data.request_token
+					})
 				})
 			})
-			if(validateWithLogin.status > 400) throw new Error ("Error session");
-			let sessionResult = await  session.json();
-		}
-		catch(error){
-			let e = error
-		}
+      .then(data => {
+        console.log("session", data);
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
 	}
 
 	render(){
