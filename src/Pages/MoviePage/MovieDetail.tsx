2bfc0891bbed1f83755f674cb15ea273c882fdc6
Slava <slava91petrushin@yandex.ru>
2021-03-11 21:11:12 +0300

    added info details

--- src/Pages/MoviePage/MovieDetail.tsx
+++ src/Pages/MoviePage/MovieDetail.tsx
@@ -1,17 +1,85 @@
-import React, { FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { CallApi } from "../../api/api";
 import { useLocation } from "react-router-dom";
+import { Table } from 'reactstrap';
+
+interface IDetails {
+	budget: number
+	vote_average: number
+	runtime: number
+	revenue: number
+	status: string
+	title: string
+	production_countries?: {name: string,}[]
+	country?: string	
+}
 
 const MovieDetail: FunctionComponent = () => {
+	const [details, setDetails] = useState<IDetails | null>(null);
 	let location = useLocation<any>();
 
 	useEffect(() => {
-		const path = location.pathname.split("/").filter((p) => p !== "").filter((d, i) => i < 2).join("/");
-		CallApi.get(path)
+		(async () => {
+			const path = location.pathname.split("/").filter((p) => p !== "").filter((d, i) => i < 2).join("/");
+			let res = await CallApi.get<IDetails>(path);
+
+			setDetails({
+				budget: res.budget,
+				vote_average: res.vote_average,
+				runtime: res.runtime,
+				revenue: res.revenue,
+				status: res.status,
+				title: res.title,
+				country: res.production_countries!.reduce((sum: string, country: any) => sum += ` ${country.name},`, ''),
+			})
+		})()
 	}, [])
 
 	return (
-		<div>MovieDetail</div>
+		<>
+			{!details && <p>Loading...</p>}
+			{details && (
+				<Table striped>
+					<thead>
+						<tr>
+							<th>Наименование</th>
+							<th>Описание</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Название</td>
+							<td>{details.title}</td>
+						</tr>
+						<tr>
+							<td>Страна</td>
+							<td>{details.country}</td>
+						</tr>
+						<tr>
+							<td>Статус</td>
+							<td>{details.status}</td>
+						</tr>
+						<tr>
+							<td>Бюджет</td>
+							<td>{details.budget} $</td>
+						</tr>
+						<tr>
+							<td>Выручка</td>
+							<td>{details.revenue}</td>
+						</tr>
+						<tr>
+							<td>Рейтинг</td>
+							<td>{details.vote_average}</td>
+						</tr>
+						<tr>
+							<td>Продолжительность</td>
+							<td>{details.runtime}</td>
+						</tr>
+					</tbody>
+				</Table>
+			)}
+		</>
+
 	)
 }
 
--- src/Pages/MoviePage/MovieTabs.tsx
+++ src/Pages/MoviePage/MovieTabs.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, NavLink} from 'react-router-dom';
+import { Route, Switch, NavLink, Redirect} from 'react-router-dom';
 import { TabContent, Nav, NavItem } from 'reactstrap';
 import MovieCredits from './MovieCredits';
 import MovieDetail from './MovieDetail';
@@ -60,6 +60,7 @@ class MovieTabs extends React.Component<IProps>{
 						<Route path={`/movie/${this.props.id}/detail`} component={MovieDetail}/>
 						<Route path={`/movie/${this.props.id}/credits`} component={MovieCredits}/>
 						<Route path={`/movie/${this.props.id}/similar`} component={MovieVideos}/>
+						<Redirect to={`/movie/${this.props.id}/detail`}/>
 					</Switch>
 				</TabContent>
     	</div>
